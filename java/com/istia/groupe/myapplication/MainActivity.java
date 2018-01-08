74f1e70975f10ec826c8f323e5e0b0c603ba534a
GACHET Colin <cgachet@terrena.fr>
2018-01-08 16:39:06 +0100

    First commit colin

--- java/com/istia/groupe/myapplication/MainActivity.java
+++ java/com/istia/groupe/myapplication/MainActivity.java
@@ -2,12 +2,14 @@ package com.istia.groupe.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+      Log.i("TAG", "Modif test git");
   }
 }
