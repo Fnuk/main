d4eb47f328101a62fed445358acf374aa7fdb774
afunk <alice.funk@consertotech.pro>
2018-01-14 00:20:28 +0100

    Ebauche de la création d'un plateau visuel + ajout de l'appel au fragment dans le mainActivity + Changement id du linear layout dans le xml du mainActivity

--- java/com/istia/groupe/myapplication/Fragment_Plateau.java
+++ java/com/istia/groupe/myapplication/Fragment_Plateau.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.GridLayout;
 
 
@@ -13,12 +14,15 @@ import android.widget.GridLayout;
  * A simple {@link Fragment} subclass.
  */
 public class Fragment_Plateau extends Fragment {
+
   View view;
   GridLayout gridDemineur = null;
+  int rows = 8;
+  int columns = 8;
+
   public Fragment_Plateau() {
     // Required empty public constructor
   }
-
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,8 +32,23 @@ public class Fragment_Plateau extends Fragment {
     view = inflater.inflate(R.layout.fragment_fragment__plateau, container, false);
 
     gridDemineur = (GridLayout) view.findViewById(R.id.GridLayoutDemineur);
+    gridDemineur.setRowCount(rows);
+    gridDemineur.setColumnCount(columns);
+
+    for(int i = 0; i < rows; i++){
+      for(int j = 0; j < columns; j++){
+        gridDemineur.addView(new Button(this.getContext()));
+      }
+    }
 
     return view;
   }
 
+  public void setRows(int rows){
+    this.rows = rows;
+  }
+
+  public void setColumns(int columns) {
+    this.columns = columns;
+  }
 }
--- java/com/istia/groupe/myapplication/MainActivity.java
+++ java/com/istia/groupe/myapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.istia.groupe.myapplication;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,7 +13,13 @@ public class MainActivity extends AppCompatActivity {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
 
-//      Test classe plateau
+      FragmentManager fragmentManager = getSupportFragmentManager();
+      FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+      Fragment_Plateau fragment = new Fragment_Plateau();
+      fragmentTransaction.add(R.id.fragment_container, fragment);
+      fragmentTransaction.commit();
+
+      //      Test classe plateau
       Plateau plat = new Plateau(15, 15, 50);
       Log.i("PLATEAU", plat.toString());
 
