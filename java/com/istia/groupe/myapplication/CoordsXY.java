ad3bd5addaaf94914c26f34affdaad84c7193cf4
Colin <cgachet@terrena.fr>
2018-01-15 11:14:49 +0100

    Ajout classe XYCoords; Ajout getters/setters classe Plateau; Ajout Singleton Plateau; Ajout getSafeZone classe Plateau

---
+++ java/com/istia/groupe/myapplication/CoordsXY.java
@@ -0,0 +1,62 @@
+package com.istia.groupe.myapplication;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by cgachet on 14/01/2018.
+ */
+
+public class CoordsXY {
+
+    public static int[][] List2Array(ArrayList<CoordsXY> xyList) {
+        int[][] xyArray = new int[xyList.size()][2];
+        for(int i=0; i<xyList.size(); i++) {
+            xyArray[i][0] = xyList.get(i).getX();
+            xyArray[i][1] = xyList.get(i).getY();
+        }
+        return xyArray;
+    }
+
+    private int X;
+    private int Y;
+
+    public CoordsXY(int x, int y) {
+        X=x;
+        Y=y;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public void setX(int x) {
+        X = x;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    public void setY(int y) {
+        Y = y;
+    }
+
+    public boolean equals(CoordsXY xy) {
+        if(xy.getX() == this.getX() && xy.getY() == this.getY()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean inArray(ArrayList<CoordsXY> arr) {
+        for(CoordsXY xy : arr) {
+            if(this.equals(xy)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
--- java/com/istia/groupe/myapplication/MainActivity.java
+++ java/com/istia/groupe/myapplication/MainActivity.java
@@ -12,8 +12,25 @@ public class MainActivity extends AppCompatActivity {
       setContentView(R.layout.activity_main);
 
 //      Test classe plateau
-      Plateau plat = new Plateau(15, 15, 50);
+      Plateau plat = Plateau.getInstance();
+      plat.init(4,4,3);
       Log.i("PLATEAU", plat.toString());
+      for(int i=0; i<plat.getRows(); i++) {
+          for(int j=0; j<plat.getRows(); j++) {
+              int[][] tab = plat.getSafeZone(i, j);
+              if(tab != null){
+                  Log.i("SAFE POSITION", "("+i+","+j+")");
+                  for(int k=0; k<tab.length; k++) {
+                      Log.i("SAFE VOISIN",  "("+tab[k][0]+","+tab[k][1]+")");
+                  }
+              } else {
+                  Log.i("BOMBE", "("+i+","+j+")");
+              }
+
+          }
+      }
+
+
 
   }
 }
